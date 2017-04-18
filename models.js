aefb0bcd03db84536559d01cd91a89746e93ce05
Camille Condrey <camillecondrey@gmail.com>
2017-04-18 10:17:21 -0600

    fix models

--- models.js
+++ models.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const blogSchema = mongoose.Schema({
 	title: {type: String, required: true},
-	content: {type: String, required: true},
-	author: {type: String, required: true},
-	created: {type: String}
-})
+	content: {type: String},
+	author: {
+		firstName: String,
+		lastName: String},
+	created: {type: Date, default: Date.now}
+});
 
 blogSchema.virtual('authorString').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim()});
